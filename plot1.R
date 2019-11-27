c754649555967171c8a4c8cc29ef4f3b8c46364b
acraig29 <ajsc.craig@gmail.com>
2019-11-27 11:49:02 +0100

    upload a revised veriosn of plot1.R file

--- plot1.R
+++ plot1.R
@@ -31,46 +31,7 @@ dev.copy(png, file="plot1.png")
 dev.off()
 
 
-#########plot 2 code
 
-
-with(plot1_df, plot( plot1_df$Time , Global_active_power, type= "l", col = c("blue"), xlab = ""   ))
-dev.copy(png, file="plot2.png")
-dev.off()
-
-#########plot 3 code
-
-
-
-
-plot(plot1_df$Time, plot1_df$Sub_metering_1, type = "l", xlab = "", ylab = "Energy Sub-metering")
-points(plot1_df$Time, plot1_df$Sub_metering_2, type = "l", col = "green")
-points(plot1_df$Time, plot1_df$Sub_metering_3, type = "l", col = "blue")
-legend("topright", lty = 1, col = c("black", "green", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-dev.copy(png, file="plot3.png")
-dev.off()
-
-#########plot 4 code
-
-
-
-par(mfrow=c(2,2))
-par(mar=c(4,4,4,4))
-
-plot(plot1_df$Time ,plot1_df$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)", col = "cyan")
-plot(plot1_df$Time, plot1_df$Voltage, type = "l", xlab = "datetime", ylab = "Voltage", col = "orange")
-
-
-plot(plot1_df$Time, plot1_df$Sub_metering_1, type = "l", xlab = "", ylab = "Energy Sub-metering")
-points(plot1_df$Time, plot1_df$Sub_metering_2, type = "l", col = "green")
-points(plot1_df$Time, plot1_df$Sub_metering_3, type = "l", col = "blue")
-legend("topright", lty = 1, col = c("black", "green", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-plot(plot1_df$Time, plot1_df$Global_reactive_power,   type = "l",  xlab = "datetime",    ylab = "Global reactive power", col = "magenta")
-
-dev.copy(png, file="plot4.png")
-dev.off()
 
 
 
